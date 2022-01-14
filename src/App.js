ef7a990a07560cda073505eec7a4b7df3a6e053b
JM_Medina <juamedrod@hotmail.es>
2022-01-14 12:30:33 +0100

    PhotoProfile card added as component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Myheader from './components/header/Myheader';
+import PhotoCard from './components/PhotoCard';
 
 function App() {
   return (
     <div className="App">
-      <Myheader />
+      {/* <Myheader /> */}
+      <div className='containerPhoto'>
+        <PhotoCard image="https://avatars.githubusercontent.com/u/53428865?v=4" />
+      </div>
     </div>
   );
 }
---
+++ src/components/PhotoCard.jsx
@@ -0,0 +1,6 @@
+const PhotoCard = (props) => {
+    return <>
+        <img src={props.image} alt="My profile Pic" className="rounded-circle" />
+    </>
+}
+export default PhotoCard;
